861452a5ae19af68c195a28a79de91aaced87724
angelicacvo <108093638+angelicacvo@users.noreply.github.com>
2025-04-22 20:53:02 -0500

    Delete python_dia2/python_dia2-main directory

--- python_dia2/python_dia2-main/ejercicio3.py
+++
@@ -1,9 +0,0 @@
-nombres = []
-nombre_usuario = input("Ingresa tu nombre ")
-
-def agregar_nombre():
-    nombres.append(nombre_usuario)
-    print(nombres)
-
-agregar_nombre()
-
--- python_dia2/python_dia2-main/ejercicio4.py
+++
@@ -1,9 +0,0 @@
-edad = int(input("Ingresa tu edad "))
-
-def validar_edad():
-    if edad < 0 or edad > 120:
-        print("Edad no válida")
-    else:
-        print("Edad válida")
-
-validar_edad()
